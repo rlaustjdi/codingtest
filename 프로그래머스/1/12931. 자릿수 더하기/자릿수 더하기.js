eb6d2fd50458171a01f1816fe81e04a6ee101457
rlaustjdi <117335037+rlaustjdi@users.noreply.github.com>
2024-10-23 13:42:44 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -0,0 +1,12 @@
+function solution(n)
+{
+    var answer = 0;
+    let m = String(n);
+    
+    for(let i =0; i<m.length; i++){
+        answer += parseInt(m[i]);
+    }
+    
+    
+    return answer;
+}
